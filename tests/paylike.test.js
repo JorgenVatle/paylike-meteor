607665b4b8930812fef10ff05a6f01c092dc6c99
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-10 19:52:02 +0300

    Assert that we can grab a merchant from the Paylike class.

--- tests/paylike.test.js
+++ tests/paylike.test.js
@@ -9,4 +9,9 @@ describe('Paylike', function() {
     it('should grab the current app identity', function() {
         expect(paylike.me.identity.id).to.not.be.undefined;
     });
+
+    it('should fetch a merchant', function() {
+        const merchant = paylike.merchants.get(testData.merchant.id);
+        expect(merchant.id).to.not.be.undefined;
+    })
 });
