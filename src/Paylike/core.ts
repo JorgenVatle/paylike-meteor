ded8f62f57e6a3b7983b8ce9b44c766ccffe2a2e
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-10 20:12:18 +0300

    Add data merge method to PaylikeCore.

--- src/Paylike/core.ts
+++ src/Paylike/core.ts
@@ -8,12 +8,27 @@ export default abstract class PaylikeCore {
     protected service: PaylikeService;
 
     /**
+     * Raw instance data.
+     */
+    protected entry: any;
+
+    /**
      * Paylike core constructor.
      *
      * @param service
      */
     protected constructor(service: PaylikeService) {
         this.service = service;
+    }
+
+    /**
+     * Merge the given data with the current instance.
+     *
+     * @param data
+     */
+    protected merge(data: any) {
+        this.entry = data;
+        Object.assign(this, data);
     }
 
 }
