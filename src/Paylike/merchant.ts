0212dd09a646e15fddd97691d1264f48e08e5f6e
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-10 20:57:53 +0300

    Use `initialize()` method for newing up new instances of PaylikeMerchant in Merhant module.

--- src/Paylike/merchant.ts
+++ src/Paylike/merchant.ts
@@ -1,19 +1,12 @@
-import PaylikeService from "./service";
+import PaylikeDataCore from "./data-core";
 
 interface MerchantData extends PaylikeApi.merchants.create.response {}
-
-type MerchantOrId = MerchantData | string;
 
 interface PaylikeMerchant extends MerchantData {
     entry: MerchantData;
 }
 
-class PaylikeMerchant {
-
-    /**
-     * Paylike Service Instance.
-     */
-    protected service: PaylikeService;
+class PaylikeMerchant extends PaylikeDataCore {
 
     /**
      * PaylikeMerchant data.
@@ -21,26 +14,14 @@ class PaylikeMerchant {
     public entry: MerchantData;
 
     /**
-     * PaylikeMerchant constructor.
-     *
-     * @param service
-     * @param merchant
-     */
-    public constructor(service: PaylikeService, merchant: MerchantData) {
-        this.service = service;
-        this.entry = merchant;
-        Object.assign(this, merchant);
-    }
-
-    /**
      * Update the current merchant.
      *
      * @param params
      */
     public update(params: PaylikeApi.merchants.update.input): PaylikeMerchant {
-        return new PaylikeMerchant(
-            this.service,
-            this.service.request('PUT', `/merchants${this.entry.id}`, params)
+        return this.initialize(
+            PaylikeMerchant,
+            this.request('PUT', `/merchants${this.entry.id}`, params)
         );
     }
 
