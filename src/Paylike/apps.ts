720f755f467e26fe3979178f21bdb6379c0f14df
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-10 22:22:34 +0300

    Add Apps class with create() method.

---
+++ src/Paylike/apps.ts
@@ -0,0 +1,17 @@
+import PaylikeCore from "./core";
+import PaylikeApp from "./app";
+
+export default class PaylikeApps extends PaylikeCore {
+
+    /**
+     * Create a new app.
+     * @param app
+     */
+    create(app: PaylikeApi.apps.create.input): PaylikeApp {
+        return <PaylikeApp>this.initialize(
+            PaylikeApp,
+            this.request('POST', '/apps', app).app
+        )
+    }
+
+}
