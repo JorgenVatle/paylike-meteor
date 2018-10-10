9b81ce32dfcecfef48b32a8ba5cfc423247af7df
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-10 19:50:17 +0300

    Add PaylikeMerchant base class.

---
+++ src/Paylike/PaylikeMerchant.ts
@@ -0,0 +1,37 @@
+import PaylikeService from "./PaylikeService";
+
+interface MerchantData extends PaylikeApi.merchants.create.response {}
+
+type MerchantOrId = MerchantData | string;
+
+interface PaylikeMerchant extends MerchantData {
+    entry: MerchantData;
+}
+
+class PaylikeMerchant {
+
+    /**
+     * Paylike Service Instance.
+     */
+    protected service: PaylikeService;
+
+    /**
+     * PaylikeMerchant data.
+     */
+    public entry: MerchantData;
+
+    /**
+     * PaylikeMerchant constructor.
+     *
+     * @param service
+     * @param merchant
+     */
+    constructor(service: PaylikeService, merchant: MerchantData) {
+        this.service = service;
+        this.entry = merchant;
+        Object.assign(this, merchant);
+    }
+
+}
+
+export default PaylikeMerchant;
