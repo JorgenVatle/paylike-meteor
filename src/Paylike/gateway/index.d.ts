32095e7b5470698f71267bb51e5f83c1e5c982fc
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-11 23:47:36 +0300

    Lighten nesting of GatewayApi type definitions.

--- src/Paylike/gateway/index.d.ts
+++ src/Paylike/gateway/index.d.ts
@@ -1,69 +1,66 @@
 import CurrencyCode = PaylikeApi.CurrencyCode;
 
+/**
+ * Gateway transactions
+ *
+ * @link https://github.com/paylike/api-docs/blob/master/gateway.md#create-a-payment
+ */
 declare module PaylikeGatewayApi {
 
+    interface card {
+        number: number,
+        core: number,
+        expiry: {
+            month: number,
+            year: number,
+        }
+    }
+
+    interface error {
+        code: number,
+        message: string,
+        client: boolean,
+        merchant: boolean,
+    }
+
     /**
-     * Gateway transactions
-     *
-     * @link https://github.com/paylike/api-docs/blob/master/gateway.md#create-a-payment
+     * Gateway Payments
      */
-    module transactions {
-
-        interface card {
-            number: number,
-            core: number,
-            expiry: {
-                month: number,
-                year: number,
+    module payment {
+        module create {
+            interface input {
+                currency: CurrencyCode,
+                amount: number,
+                card: card,
             }
-        }
 
-        interface error {
-            code: number,
-            message: string,
-            client: boolean,
-            merchant: boolean,
-        }
-
-        /**
-         * Gateway Payments
-         */
-        module payment {
-            module create {
-                interface input {
-                    currency: CurrencyCode,
-                    amount: number,
-                    card: card,
-                }
-
-                interface response {
-                    transaction: {
-                        id: string,
-                    }
-                }
-
-                interface errorResponse extends error {
-                    response: response,
+            interface response {
+                transaction: {
+                    id: string,
                 }
             }
+
+            interface errorResponse extends error {
+                response: response,
+            }
         }
+    }
 
-        /**
-         * Gateway cards
-         */
-        module card {
-            module tokenize {
-                type input = card;
+    /**
+     * Gateway cards
+     */
+    module card {
+        module tokenize {
+            type input = card;
 
-                interface response {
-                    card: {
-                        id: string,
-                    }
+            interface response {
+                card: {
+                    id: string,
                 }
+            }
 
-                interface errorResponse extends error {
-                    response: response,
-                }
+            interface errorResponse extends error {
+                response: response,
             }
         }
     }
