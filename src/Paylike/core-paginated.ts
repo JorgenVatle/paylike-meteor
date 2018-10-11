c8cb852138044e8442322719f4a9be03f7a0fcca
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-11 03:49:52 +0300

    Add paginated core module.

---
+++ src/Paylike/core-paginated.ts
@@ -0,0 +1,41 @@
+import PaylikeUser from "./modules/user";
+import PaylikeCore from "./core";
+import PaylikeDataCore from "./core-data";
+
+export interface Paginated {
+    path: string,
+}
+
+export default abstract class PaylikeCorePaginated extends PaylikeCore implements Paginated {
+
+    /**
+     * Pagination path
+     */
+    path: string;
+
+    /**
+     * Alternative service object
+     */
+    alternativeService?: any;
+
+    /**
+     * Class each paginated entry should be initialized in.
+     *
+     * E.g. if `Merchants` extends this class, this value
+     * would be `Merchant`.
+     */
+    singularModule: PaylikeDataCore;
+
+    /**
+     * Build an array of users.
+     *
+     * @param query
+     */
+    public fetch(query: PaylikeApi.PaginationQuery = { limit: 50 }) {
+        return this.initializeList(
+            this.singularModule,
+            this.request('GET', this.path, query),
+            this.alternativeService
+        );
+    }
+}
