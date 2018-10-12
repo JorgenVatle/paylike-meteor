ff4d70b8a383ef5031872aeb2b8eb6501a2b86a9
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-12 06:19:48 +0300

    Add save() method to PaylikeCards class.

--- src/Paylike/api/modules/cards.ts
+++ src/Paylike/api/modules/cards.ts
@@ -22,6 +22,16 @@ export default class PaylikeCards extends PaylikeMerchantCorePaginated {
         return <PaylikeCard>this.findByPath(this.buildPath(cardId), 'card');
     }
 
+    /**
+     * Save a card using a transaction ID.
+     *
+     * @param data
+     */
+    public save(data: PaylikeApi.cards.save.input) {
+        const card = this.request('POST', this.merchant.buildPath('/cards'), data).card;
+        return this.find(card.id);
+    }
+
     // This module endpoint does not have any pagination functionality.
     // So we set the inherited pagination method types to undefined to prevent any confusion.
     public fetch: undefined;
