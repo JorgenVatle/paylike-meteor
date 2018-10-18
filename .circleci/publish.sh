8348cb31047ed2b15ce6afc0b315c630bb9c583e
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-18 18:52:23 +0300

    Add NPM publish step to publish script.

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -3,8 +3,13 @@
 COMMIT_MESSAGE=$(ci-github-utils get-comment -h "$CIRCLE_SHA1" -o "$CIRCLE_PROJECT_USERNAME" -r "$CIRCLE_PROJECT_REPONAME")
 
 if [[ "$COMMIT_MESSAGE" =~ ^v[0-9]+\.[0-9]+\.[0-9]+ ]] && [ "$CIRCLE_BRANCH" == "master" ]; then
+    # Atmosphere
     rm -rf node_modules
     METEOR_SESSION_FILE=meteor_session.json meteor publish
+
+    # NPM
+    node sync-version.js
+    npm publish
 else
  echo "Skipping package publish step"
 fi
