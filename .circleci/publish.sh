1ca79824799b1fc4d4ee524714c065d52ebb095c
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-18 19:19:36 +0300

    Push Meteor session into session file within publish.sh instead of CircleCI config.

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -4,6 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 COMMIT_MESSAGE=$(ci-github-utils get-comment -h "$CIRCLE_SHA1" -o "$CIRCLE_PROJECT_USERNAME" -r "$CIRCLE_PROJECT_REPONAME")
 
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/repo/.npmrc
+echo $METEOR_SESSION > meteor_session.json
 
 if [[ "$COMMIT_MESSAGE" =~ ^v[0-9]+\.[0-9]+\.[0-9]+ ]] && [ "$CIRCLE_BRANCH" == "master" ]; then
     # Atmosphere
