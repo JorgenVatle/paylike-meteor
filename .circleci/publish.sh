7d37dc31e393c3b9c2ecba6b868d2af52f9b2e29
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-18 19:31:03 +0300

    Run npm version sync before publishing the Meteor package.

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -4,10 +4,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 COMMIT_MESSAGE=$(ci-github-utils get-comment -h "$CIRCLE_SHA1" -o "$CIRCLE_PROJECT_USERNAME" -r "$CIRCLE_PROJECT_REPONAME")
 
 if [[ "$COMMIT_MESSAGE" =~ ^v[0-9]+\.[0-9]+\.[0-9]+ ]] && [ "$CIRCLE_BRANCH" == "master" ]; then
+
+    node $DIR/sync-version.js
+
     rm -rf node_modules
     METEOR_SESSION_FILE=meteor_session.json meteor publish
 
-    node $DIR/sync-version.js
     npm publish
 else
  echo "Skipping package publish step"
