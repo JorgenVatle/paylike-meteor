e37780586ea078079399c5d74c289493e55a74ee
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-18 19:18:55 +0300

    Set NPM token in publish.sh.

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
 COMMIT_MESSAGE=$(ci-github-utils get-comment -h "$CIRCLE_SHA1" -o "$CIRCLE_PROJECT_USERNAME" -r "$CIRCLE_PROJECT_REPONAME")
+
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/repo/.npmrc
 
 if [[ "$COMMIT_MESSAGE" =~ ^v[0-9]+\.[0-9]+\.[0-9]+ ]] && [ "$CIRCLE_BRANCH" == "master" ]; then
     # Atmosphere
