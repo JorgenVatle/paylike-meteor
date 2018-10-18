954041ea27f16531a10081d2b5c7a97d81b1a15b
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-18 18:50:39 +0300

    Add Atmosphere->NPM version sync script to .circleci directory.

---
+++ .circleci/sync-version.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const jsonPath = '../package.json';
+const packageJson = require(jsonPath);
+const packageJs = require('../package.js');
+
+packageJson.version = packageJs.details.version.replace(/v/g, '');
+packageJson.private = false;
+
+fs.writeFile(jsonPath, JSON.stringify(packageJson, null, 2), (err) => {
+    if (err) throw err;
+    console.log('Set package.json version to:', packageJson.version);
+});
