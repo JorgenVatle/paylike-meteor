3b07d22d07a097afcc18f7e7ccdae318fbe48137
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-18 09:21:55 +0300

    Export PaylikeFraudAlertData interface.

--- src/Paylike/api/modules/fraud-alert.ts
+++ src/Paylike/api/modules/fraud-alert.ts
@@ -1,6 +1,6 @@
 import PaylikeCoreData from "../core-data";
 
-interface PaylikeFraudAlertData extends PaylikeApi.fraud.fraud {}
+export interface PaylikeFraudAlertData extends PaylikeApi.fraud.fraud {}
 
 interface PaylikeFraudAlert extends PaylikeFraudAlertData {
     entry: PaylikeFraudAlertData,
