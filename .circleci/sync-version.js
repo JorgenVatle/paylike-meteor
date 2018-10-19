d28115ffe93cf1bc84064c09976b0673f5434fa4
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-19 11:42:27 +0300

    Use package.json as version master for sync-version.js.

--- .circleci/sync-version.js
+++ .circleci/sync-version.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
-const packageJs = require('../package.js');
 
 const jsonPath = __dirname + '/../package.json';
+const jsPath = __dirname + '/../package.js';
 const packageJson = require(jsonPath);
 
-packageJson.version = packageJs.details.version;
-packageJson.private = false;
+const rawJs = fs.readFileSync(jsPath, 'utf8')
+    .replace(/version: '\d+\.\d+\.\d+'/, `version: '${packageJson.version}'`);
 
-fs.writeFile(jsonPath, JSON.stringify(packageJson, null, 2), (err) => {
+fs.writeFile(jsPath, rawJs, (err) => {
     if (err) throw err;
-    console.log('Set package.json version to:', packageJson.version);
+    console.log('Set package.js version to:', packageJson.version);
 });
