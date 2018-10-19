b3d052e51052d4694a5923d3bda4a32ca6b81dee
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-19 11:23:52 +0300

    v1.2.1

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ var Package = Package || {
 var module = module || {};
 var details = {
     name: 'jorgenvatle:paylike',
-    version: '1.2.0',
+    version: '1.2.1',
     summary: 'Meteor HTTP wrapper for PayLike\'s REST API',
     git: 'https://github.com/JorgenVatle/paylike-meteor.git',
     documentation: 'README.md'
