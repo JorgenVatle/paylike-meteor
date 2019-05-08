a4d0538f55086b7db0ee41a4951974360fea7174
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-08 14:55:29 +0300

    Export PaylikeApi module from package index.

--- src/Paylike/index.ts
+++ src/Paylike/index.ts
@@ -1,6 +1,11 @@
 import PaylikeService from "./api/service";
 import Gateway from "./gateway/gateway";
+import { PaylikeApi } from './api/PaylikeApi';
 
 export default PaylikeService;
 
 export const PaylikeGateway = Gateway;
+
+export {
+    PaylikeApi,
+};
